380e69f3d03d1b23d91fafa26d254dcc93bec7d0
Nur Arini <nurarini0302@gmail.com>
2024-11-09 06:24:50 +0700

    initial project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Hello world</h1>
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
